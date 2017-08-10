816780cb7e1954e3d4123d2f31c2470d7186d405
Jackie Gu <jacqueline_gu@brown.edu>
2017-08-10 14:39:40 -0700

    issues with alignment of text & graphics but smooth scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,12 @@
 $(document).ready(function() {
 	var prev_scroll = $(window).scrollTop();
 	var scrolly_pos = $('.interactive-container').position().top;
-	console.log(scrolly_pos);
+	//console.log(scrolly_pos);
 
 $(window).scroll(function() {
 	var pos = $(this).scrollTop();
 	// var intro_pos = $('.one').offset().top;
-	console.log(pos);
+	//console.log(pos);
 	// console.log("first img: " + intro_pos);
 
 	if (prev_scroll < $(window).scrollTop()) {
@@ -30,28 +30,31 @@ $(window).scroll(function() {
 		console.log("hi");
 		$('.four').addClass("hide");
 		$('.five').removeClass("hide");
-	} else if (prev_scroll > scrolly_pos+700 && prev_scroll < scrolly_pos+900) {
-		$('.five').addClass("hide");
+	} else if (prev_scroll > scrolly_pos+700) {
+		//$('.five').addClass("hide");
+		$('.five').css({ position: 'absolute' , top: (scrolly_pos+800).toString() + 'px'});
 	}
 
 } else {
-	console.log("scrolling up");
-	if (prev_scroll < scrolly_pos+900 && prev_scroll > scrolly_pos+700) {
-		$('.five').removeClass("hide");
-	} else if (prev_scroll < scrolly_pos+700 && prev_scroll > scrolly_pos+500) {
+	if (prev_scroll < scrolly_pos+700 && prev_scroll > scrolly_pos+500) {
+		$('.img').removeClass("hide");
+		$('.img').css({ position: 'fixed',top: '100px' });
+	} else if (prev_scroll < scrolly_pos+500 && prev_scroll > scrolly_pos+300) {
+		console.log("got here");
 		$('.five').addClass("hide");
 		$('.four').removeClass("hide");
-	} else if (prev_scroll < scrolly_pos+500 && prev_scroll > scrolly_pos+300) {
+	} else if (prev_scroll < scrolly_pos+300 && prev_scroll > scrolly_pos+100) {
 		$('.four').addClass("hide");
 		$('.three').removeClass("hide");
-	} else if (prev_scroll < scrolly_pos+300 && prev_scroll > scrolly_pos+100) {
+	} else if (prev_scroll < scrolly_pos+100 && prev_scroll > scrolly_pos-100) {
 		$('.three').addClass("hide");
 		$('.two').removeClass("hide");
-	} else if (prev_scroll < scrolly_pos+100 && prev_scroll > scrolly_pos-100) {
+	} else if (prev_scroll < scrolly_pos-100 && prev_scroll > scrolly_pos-300) {
 		$('.two').addClass("hide");
 		$('.one').removeClass("hide");
-	} else if (prev_scroll < scrolly_pos-100) {
 		$('.img').css({ position: 'absolute' , top: scrolly_pos.toString() + 'px'});
+	} else if (prev_scroll < scrolly_pos-300) {
+		//$('.img').css({ position: 'absolute' , top: scrolly_pos.toString() + 'px'});
 	}
 }
 prev_scroll = pos;
