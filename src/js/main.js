2a16f8f1ec273a64896b0394eb0fda58fe8fee3c
Jackie Gu <jacqueline_gu@brown.edu>
2017-08-10 14:11:33 -0700

    added lorem ipsem text for placement

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@
 
 $(document).ready(function() {
 	var prev_scroll = $(window).scrollTop();
+	var scrolly_pos = $('.interactive-container').position().top;
+	console.log(scrolly_pos);
 
 $(window).scroll(function() {
 	var pos = $(this).scrollTop();
@@ -12,41 +14,43 @@ $(window).scroll(function() {
 	if (prev_scroll < $(window).scrollTop()) {
 		console.log("scrolling down");
 
-	if (prev_scroll > 200 && prev_scroll < 400) {
+	if (prev_scroll > scrolly_pos-100 && prev_scroll < scrolly_pos+100) {
 		$('.one').addClass("hide");
 		$('.two').removeClass("hide");
-	} else if (prev_scroll > 400 && prev_scroll < 600) {
+		$('.img').css({ position: 'fixed',top: '100px' });
+	} else if (prev_scroll > scrolly_pos+100 && prev_scroll < scrolly_pos+300) {
 		console.log("hi");
 		$('.two').addClass("hide");
 		$('.three').removeClass("hide");
-	} else if (prev_scroll > 600 && prev_scroll < 800) {
+	} else if (prev_scroll > scrolly_pos+300 && prev_scroll < scrolly_pos+500) {
 		console.log("hi");
 		$('.three').addClass("hide");
 		$('.four').removeClass("hide");
-	} else if (prev_scroll > 800 && prev_scroll < 1000) {
+	} else if (prev_scroll > scrolly_pos+500 && prev_scroll < scrolly_pos+700) {
 		console.log("hi");
 		$('.four').addClass("hide");
 		$('.five').removeClass("hide");
-	} else if (prev_scroll > 1000 && prev_scroll < 1200) {
+	} else if (prev_scroll > scrolly_pos+700 && prev_scroll < scrolly_pos+900) {
 		$('.five').addClass("hide");
 	}
 
 } else {
 	console.log("scrolling up");
-	if (prev_scroll < 1200 && prev_scroll > 800) {
+	if (prev_scroll < scrolly_pos+900 && prev_scroll > scrolly_pos+700) {
 		$('.five').removeClass("hide");
-	} else if (prev_scroll < 800 && prev_scroll > 600) {
+	} else if (prev_scroll < scrolly_pos+700 && prev_scroll > scrolly_pos+500) {
 		$('.five').addClass("hide");
 		$('.four').removeClass("hide");
-	} else if (prev_scroll < 600 && prev_scroll > 400) {
+	} else if (prev_scroll < scrolly_pos+500 && prev_scroll > scrolly_pos+300) {
 		$('.four').addClass("hide");
 		$('.three').removeClass("hide");
-	} else if (prev_scroll < 400 && prev_scroll > 200) {
+	} else if (prev_scroll < scrolly_pos+300 && prev_scroll > scrolly_pos+100) {
 		$('.three').addClass("hide");
 		$('.two').removeClass("hide");
-	} else if (prev_scroll < 200 && prev_scroll > 0) {
+	} else if (prev_scroll < scrolly_pos+100 && prev_scroll > scrolly_pos-100) {
 		$('.two').addClass("hide");
 		$('.one').removeClass("hide");
+		$('.img').css({ position: 'absolute' , top: scrolly_pos.toString() + 'px'});
 	}
 }
 prev_scroll = pos;
