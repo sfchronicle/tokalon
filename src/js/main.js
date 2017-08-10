85b029beb6ce3d6c502a038b4581472c2fac4724
Jackie Gu <jacqueline_gu@brown.edu>
2017-08-10 13:35:47 -0700

    scrolly works primitively

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,57 @@
 // require("./lib/social");
 
+$(document).ready(function() {
+	var prev_scroll = $(window).scrollTop();
+
 $(window).scroll(function() {
 	var pos = $(this).scrollTop();
-	var intro_pos = $('.one').offset().top;
+	// var intro_pos = $('.one').offset().top;
 	console.log(pos);
-	console.log("first img: " + intro_pos);
+	// console.log("first img: " + intro_pos);
 
-	if (pos > 400 && pos < 800) {
+	if (prev_scroll < $(window).scrollTop()) {
+		console.log("scrolling down");
+
+	if (prev_scroll > 200 && prev_scroll < 400) {
 		$('.one').addClass("hide");
 		$('.two').removeClass("hide");
-	} else if (pos > 800 && pos < 1200) {
+	} else if (prev_scroll > 400 && prev_scroll < 600) {
 		console.log("hi");
 		$('.two').addClass("hide");
 		$('.three').removeClass("hide");
-	} else if (pos > 1200 && pos < 1600) {
+	} else if (prev_scroll > 600 && prev_scroll < 800) {
 		console.log("hi");
 		$('.three').addClass("hide");
 		$('.four').removeClass("hide");
-	} else if (pos > 1600 && pos < 2000) {
+	} else if (prev_scroll > 800 && prev_scroll < 1000) {
 		console.log("hi");
 		$('.four').addClass("hide");
 		$('.five').removeClass("hide");
-	} else if (pos > 2000 && pos < 2400) {
-		console.log("hi");
+	} else if (prev_scroll > 1000 && prev_scroll < 1200) {
 		$('.five').addClass("hide");
 	}
+
+} else {
+	console.log("scrolling up");
+	if (prev_scroll < 1200 && prev_scroll > 800) {
+		$('.five').removeClass("hide");
+	} else if (prev_scroll < 800 && prev_scroll > 600) {
+		$('.five').addClass("hide");
+		$('.four').removeClass("hide");
+	} else if (prev_scroll < 600 && prev_scroll > 400) {
+		$('.four').addClass("hide");
+		$('.three').removeClass("hide");
+	} else if (prev_scroll < 400 && prev_scroll > 200) {
+		$('.three').addClass("hide");
+		$('.two').removeClass("hide");
+	} else if (prev_scroll < 200 && prev_scroll > 0) {
+		$('.two').addClass("hide");
+		$('.one').removeClass("hide");
+	}
+}
+prev_scroll = pos;
+})
+
 })
 
 
